f2563e756df9d5e79e0c91155bd7a85f9d76cab2
Alpha Bah <alphabah13@icloud.com>
2024-11-06 22:06:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ function toggleMenu() {
     icon.classList.toggle("open");
   }
   
+  const phrases = ["Student", "Creative", "Programmer", "Athlete"];
+let currentPhraseIndex = 0;
+let currentLetterIndex = 0;
+const typingText = document.getElementById("typing-text");
+
+function type() {
+  const currentPhrase = phrases[currentPhraseIndex];
+  typingText.innerHTML = currentPhrase.slice(0, currentLetterIndex);
+  currentLetterIndex++;
+
+  if (currentLetterIndex > currentPhrase.length) {
+    currentLetterIndex = 0;
+    currentPhraseIndex = (currentPhraseIndex + 1) % phrases.length;
+    setTimeout(type, 1000);
+  } else {
+    setTimeout(type, 100);
+  }
+}
+
+type();
